0707d1bb96eadc97350428fc4455eb4beca7bf6d
Animesh Ramesh <animeshramesh@gmail.com>
2013-09-07 22:29:55 +0530

    Update code4.cpp

--- 07-09-2013/4/code4.cpp
+++ 07-09-2013/4/code4.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<algorithm>	//needed for sorting the array
-
+#include<cstdio>
 using namespace std;
 
 int main()
@@ -10,19 +10,24 @@ int main()
 	{	int students;
 		cin>>students;
 		vector<int> ref;	
-		
+		set<int> ref2;
 		// Vectors are 'super' arrays. They can do a lot of cool stuff which arrays cant. :P
 		
 		for (int i=0; i<students;i++)
 		{	int k;
 			cin>>k;
 			ref.push_back(k);
+			ref2.push_back(k);
 		}
 		
 		sort(ref.begin(),ref.end());	//Sorting the input 
 		
 		if (students < 3)				// Its going to be in an Arithmetic Progression series anyway if no. of students < 3
 		{	printf("YES\n");
+			continue;
+		}
+		if (ref.size()!=ref2.size())
+		{	cout<<"NO\n";
 			continue;
 		}
 		int flag = 0;
